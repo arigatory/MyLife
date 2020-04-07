6f2d50cd0e0186e5b2940eb45c9cb85dfa6e9530
Панченко Иван Иванович <Panchenko_II@cbs.local>
2020-04-08 02:58:39 +0300

    Add project files.

---
+++ MyLife/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyLife
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyTasks tasks = new MyTasks();
+        bool b1 = false;
+        bool b2 = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            tasks.all.Add((MyTask)"Test 2!");
+            tasks.all.Add((MyTask)"Test 3!");
+            DataContext = tasks.all;
+            grdSort.Visibility = Visibility.Collapsed;
+            
+            
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            tasks.all.Add((MyTask)txtBox.Text);
+            txtBox.SelectAll();
+            lstOfTasks.Items.Refresh();
+        }
+
+        private void txtBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                btnAdd_Click(sender, e);
+            }
+        }
+
+
+        private void btnDone_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstOfTasks.SelectedIndex<0 || lstOfTasks.SelectedIndex >= tasks.all.Count)
+            {
+                return;
+            }
+            tasks.all.RemoveAt(lstOfTasks.SelectedIndex);
+            lstOfTasks.Items.Refresh();
+        }
+
+        private void btnSort_Click(object sender, RoutedEventArgs e)
+        {
+            grdSort.Visibility = Visibility.Visible;
+            btn1.Content = tasks.SortStepStart().Item1.Name;
+            btn2.Content = tasks.SortStepStart().Item2.Name;
+
+            //MyTask mostTask;
+            //int mostIndex;
+            //for (int i = 0; i < tasks.all.Count; i++)
+            //{
+            //    mostTask = tasks.all[i];
+            //    mostIndex = i;
+                
+            //    for (int j = 0; j < tasks.all.Count; j++)
+            //    {
+            //        if (b2)
+            //        {
+            //            mostTask = tasks.all[j];
+            //            mostIndex = j;
+            //            b2 = false;
+            //        } else
+            //        {
+            //            b1 = false;
+            //        }
+            //        MyTask temp = tasks.all[i];
+            //        tasks.all[i] = mostTask;
+            //        tasks.all[mostIndex] = temp;
+            //    }
+            //}
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            if (tasks.SortStepEnd(true))
+            {
+                grdSort.Visibility = Visibility.Collapsed;
+            } else
+            {
+                lstOfTasks.Items.Refresh();
+                btn1.Content = tasks.SortStepStart().Item1.Name;
+                btn2.Content = tasks.SortStepStart().Item2.Name;
+            }
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            if (tasks.SortStepEnd(false))
+            {
+                grdSort.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                lstOfTasks.Items.Refresh();
+                btn1.Content = tasks.SortStepStart().Item1.Name;
+                btn2.Content = tasks.SortStepStart().Item2.Name;
+            }
+        }
+    }
+}
---
+++ MyLife/MyTasks.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLife
+{
+    class MyTasks
+    {
+        private bool sorted = false;
+        private int _current_i = 0;
+        private int _current_j = 1;
+        private int _max_index = 0;
+
+        public List<MyTask> all { get; set; }
+        public MyTasks()
+        {
+            all = new List<MyTask>();
+            all.Add(new MyTask("Test 1!"));
+        }
+        public (MyTask,MyTask) SortStepStart()
+        {
+            MyTask t1 = all[_current_i];
+            MyTask t2 = all[_current_j];
+            return (t1, t2);
+        }
+        public bool SortStepEnd(bool first)
+        {
+            if (first)
+            {
+                
+            } else
+            {
+                _max_index = _current_j;
+            }
+            _current_j++;
+            if (_current_j >= all.Count-1)
+            {
+                MyTask temp = all[_current_i];
+                all[_current_i] = all[_max_index];
+                all[_max_index] = temp;
+                _current_i++;
+                _current_j = _current_i + 1;
+                _max_index = _current_i;
+                if (_current_i == all.Count - 1)
+                {
+                    _current_i = 0;
+                    _current_j = 1;
+                    _max_index = 0;
+                    sorted = true;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    class MyTask
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Due { get; set; }
+        public MyTask(string s)
+        {
+            Name = s;
+            Created = DateTime.Now;
+        }
+        public override string ToString()
+        {
+            return $"{Name} created {Created.ToShortDateString()}";
+        }
+        public static explicit operator MyTask(string s) => new MyTask(s);
+    }
+}
